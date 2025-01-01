68da6de222dd15b9cc68b51ec70da3320f45dbf6
ShahriarAlam47 <johnshah2022@gmail.com>
2025-01-01 23:11:14 +0600

    second commit

--- restaurant.py
+++ restaurant.py
@@ -4,15 +4,24 @@ class Restaurant:
         self.cuisine_type = cuisine_type
 
     def describe_restaurant(self):
-        print(f"Our restaurant name is {self.restaurant_name}.")
-        print(f"We serve {self.cuisine_type} in our restaurant.")
+        print(f"Our restaurant name is '{self.restaurant_name}'.")
+        print(f"We serve '{self.cuisine_type}' in our restaurant.")
 
     def open_restaurant(self):
         print("The restaurant is open at 10 am.")
 
-restaurant = Restaurant("Bangla Hotel", "Buffet")
+restaurant = Restaurant("Bangla Hotel", "Rice")
 print(restaurant.restaurant_name)
 print(restaurant.cuisine_type)
 
 restaurant.describe_restaurant()
 restaurant.open_restaurant()
+
+# additional description for 3 restaurant
+restaurant1 = Restaurant("Khan Snacks", "Biriyani")
+restaurant2 = Restaurant("Hajir Biriyani", "Biriyani")
+restaurant3 = Restaurant("Swampa Restaurant", "Fast Food")
+
+restaurant1.describe_restaurant()
+restaurant2.describe_restaurant()
+restaurant3.describe_restaurant()
